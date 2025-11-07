2010416c230e7f1e516a3f8a5240036233aed038
Gabriel Collin <gabicollin91910@gmail.com>
2025-11-08 00:11:08 +0100

    complete main.cpp

--- POO/Prosit 1/Main/src/main.cpp
+++ POO/Prosit 1/Main/src/main.cpp
@@ -1,1 +1,12 @@
 #include "point.hpp"
+#include "vector.hpp"
+#include "segment.hpp"
+#include <cstdlib>
+
+int main() {
+  Point p1(0.,0.), p2(1.,2.);
+  Vector v1(1.,1.), v2(2.,4.);
+  Segment s1(p1,v1), s2(p1,v1);
+
+  return EXIT_SUCCESS;
+}

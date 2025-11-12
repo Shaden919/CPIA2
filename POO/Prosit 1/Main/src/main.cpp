0967f59c9f56cc6e0f0e5399d99f19f70a9dc3e0
gabriel Collin <gabicollin91910@gmail.com>
2025-11-12 14:02:23 +0100

    add a cout in main

--- POO/Prosit 1/Main/src/main.cpp
+++ POO/Prosit 1/Main/src/main.cpp
@@ -8,6 +8,6 @@ int main() {
   Point p1(0.,0.), p2(1.,2.);
   Vector v1(1.,1.), v2(2.,4.);
   Segment s1(p1,v1), s2(p1,v1);
-  std::cout << "Code ok";
+  std::cout << "Code ok" << std::endl;
   return EXIT_SUCCESS;
 }

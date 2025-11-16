0d56f3cab504c831f3a01d036714e257ba555c36
gabriel Collin <gabicollin91910@gmail.com>
2025-11-16 15:49:15 +0100

    add xOr not tested

---
+++ POO/Prosit 2/xOr/build/CMakeFiles/exe.dir/src/crypt.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/exe.dir/src/crypt.cpp.o: \
+ /root/Bloque/POO/Prosit\ 2/xOr/src/crypt.cpp /usr/include/stdc-predef.h
---
+++ POO/Prosit 2/xOr/src/file.cpp
@@ -0,0 +1,78 @@
+#include "file.hpp"
+#include <fstream>
+#include <cstdio>
+#include <iostream>
+
+File::File(const string& name,const string& path) {
+    this->name = name;
+    this->path = path.empty() ? name : path ;
+}
+
+File::~File() {}
+
+void File::setName(string name) {
+    this->name = name;
+}
+void File::setPath(string path) {
+    this->path = path;
+}
+string File::getName() {
+    return this->name;
+}
+string File::getPath() {
+    return this->path;
+}
+
+string File::read() const{
+    ifstream file(this->path.c_str(), ios::in);
+    if(file.fail()) {
+        cerr << "Erreur d'ouverture du fichier \"" << this->name << "\"\n";
+        return "";
+    }
+
+    string output;
+    string ligne;
+
+    while (getline(file,ligne)) output += ligne + "\n";
+    
+    cout << output << endl;
+    return output;  
+    
+}
+
+void File::write(string ligne) const{
+    
+    ofstream file(this->path.c_str(),ios::out | ios::ate);
+    if(file.fail()) {
+        cerr << "Erreur d'ouverture du fichier \"" << this->name << "\"\n";
+        return;
+    }
+
+    file << ligne << endl;
+    file.close();
+}
+
+bool File::del() {
+    if (remove(this->path.c_str()) == 0)
+    {
+        cout << "fichier supprimer\n";
+        this->name = "";
+        this->path = "";
+        return 0;
+    }
+    cout << "fichier inexistant";
+    return 1;
+}
+
+bool File::rename(string name) {
+    File newFile(name,name + ".txt");
+    ifstream file(this->path.c_str(), ios::in);
+    if(file.fail()) {
+        cerr << "Erreur d'ouverture du fichier \"" << this->name << "\"\n";
+        return 1;
+    }
+    string ligne;
+    while (getline(file,ligne))newFile.write(ligne);
+    this->del();
+    return 0;
+}
